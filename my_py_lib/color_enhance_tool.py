f6ef73cce4ea7dd0d1eca7157ceecc34c2bc897c
onesixth <1944376797@qq.com>
2020-11-23 11:32:26 +0800

    Update color_enhance_tool.py

--- my_py_lib/color_enhance_tool.py
+++ my_py_lib/color_enhance_tool.py
@@ -27,13 +27,29 @@ def _get_random_params(x_range: Union[int, float, Iterable], not_allow_negative_
     return y
 
 
-def random_adjust_HSV(img, h_range=0., s_range=0., v_range=0.):
+def random_params_adjust_HSV(h_range=0., s_range=0., v_range=0.):
+    '''
+    随机参数用于图像的HSL空间进行调整
+    :param h_range: 调整色调，值域[-1, 1]
+    :param s_range: 调整对比度，值域[-1, 1]
+    :param v_range: 调整亮度，值域[-1, 1]
+    :return:
+    '''
+    hsv_add = np.array([_get_random_params(h_range),
+                        _get_random_params(s_range),
+                        _get_random_params(v_range)])
+    return hsv_add
+
+
+def adjust_HSV(img, h_add, s_add, v_add):
     '''
     在图像的HSL空间进行调整
     :param img: 输入的图像
     :param h_range: 调整色调，值域[-1, 1]
     :param s_range: 调整对比度，值域[-1, 1]
     :param v_range: 调整亮度，值域[-1, 1]
+    :param return_params: 是否返回随机参数
+    :param fix_params: 使用固定参数参数进行变换
     :return:
     '''
     assert img.dtype == np.uint8
@@ -41,10 +57,7 @@ def random_adjust_HSV(img, h_range=0., s_range=0., v_range=0.):
     hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV_FULL)
     hsv = np.asarray(hsv, np.float32) / 255.
 
-    hsv_add = np.array([_get_random_params(h_range),
-                        _get_random_params(s_range),
-                        _get_random_params(v_range)])
-
+    hsv_add = [h_add, s_add, v_add]
     hsv = hsv + np.reshape(hsv_add, [1, 1, 3])
     # 注意色调是一个圆，可以循环的，所以使用求余
     # 令小于0.的H值+1得到正数，避免求余歧义
@@ -53,6 +66,53 @@ def random_adjust_HSV(img, h_range=0., s_range=0., v_range=0.):
     hsv = np.clip(hsv, 0, 1.) * 255.
     hsv = hsv.astype(np.uint8)
     img = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB_FULL)
+
+    return img
+
+
+def random_adjust_HSV(img, h_range=0., s_range=0., v_range=0.):
+    '''
+    随机参数用于图像的HSL空间进行调整
+    :param img: 输入图像
+    :param h_range: 调整色调，值域[-1, 1]
+    :param s_range: 调整对比度，值域[-1, 1]
+    :param v_range: 调整亮度，值域[-1, 1]
+    :return:
+    '''
+    params = random_params_adjust_HSV(h_range, s_range, v_range)
+    img = adjust_HSV(img, *params)
+    return img
+
+
+def random_params_adjust_contrast_brightness(contrast_range: Union[float, Iterable]=0., brightness_range: Union[float, Iterable]=0.):
+    '''
+    获得对图像的对比度和亮度进行随机调整的参数
+    :param img: 输入的图像
+    :param contrast_range: 调整对比度，值域为(-1., +inf)
+    :param brightness_range: 调整亮度，值域为(-1., +inf)
+    :return:
+    '''
+    contrast = _get_random_params(contrast_range, not_allow_negative_num=True) + 1
+    brightness = _get_random_params(brightness_range)
+    return contrast, brightness
+
+
+def adjust_contrast_brightness(img: np.ndarray, contrast: float, brightness: float):
+    '''
+    对图像的对比度和亮度进行调整
+    :param img: 输入的图像，要求值域为[0, 255]
+    :param contrast: 调整对比度，值域为(-1., +inf)
+    :param brightness: 调整亮度，值域为(-1., +inf)
+    :return:
+    '''
+    assert img.dtype == np.uint8
+    assert img.ndim == 3 and img.shape[2] == 3
+    img = np.asarray(img, np.float32) / 255.
+
+    img = contrast * img + brightness
+    img = np.clip(img, 0, 1.) * 255.
+    img = img.astype(np.uint8)
+
     return img
 
 
@@ -64,16 +124,8 @@ def random_adjust_contrast_brightness(img, contrast_range: Union[float, Iterable
     :param brightness_range: 调整亮度，值域为(-1., +inf)
     :return:
     '''
-    assert img.dtype == np.uint8
-    assert img.ndim == 3 and img.shape[2] == 3
-    img = np.asarray(img, np.float32) / 255.
-
-    contrast = _get_random_params(contrast_range, not_allow_negative_num=True) + 1
-    brightness = _get_random_params(brightness_range)
-
-    img = contrast * img + brightness
-    img = np.clip(img, 0, 1.) * 255.
-    img = img.astype(np.uint8)
+    params = random_adjust_contrast_brightness_params(contrast_range, brightness_range)
+    img = adjust_contrast_brightness(img, *params)
     return img
 
 
