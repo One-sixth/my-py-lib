9095b22932ff0d2f72cf2c4dd7e3f6f2e6ddc9d0
OneSixth <One-sixth@users.noreply.github.com>
2022-06-21 12:12:01 +0800

    add install cmd

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup  # , find_packages
+
+ver_str = '0.0.1'
+
+setup(
+    name='my-py-lib',
+    version=ver_str,
+    description='Personally used python toolkit.',
+    author='onesixth',
+    author_email='noexist@noexist.noexist',
+    url='https://github.com/One-sixth/my-py-lib',
+    install_requires=[],
+    entry_points={'console_scripts': []},
+    packages=['my_py_lib'],
+    package_data={},
+)
