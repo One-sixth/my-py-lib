c196d9e5b8389973d372498a201bc81a60ea4425
OneSixth <One-sixth@users.noreply.github.com>
2022-12-02 21:05:50 +0800

    new func load_ak_from_pandas and load_ak_from_excel in awkward_tool

--- my_py_lib/awkward_tool.py
+++ my_py_lib/awkward_tool.py
@@ -1,13 +1,17 @@
-import io
 import awkward as ak
 import csv
+import pandas
 
 
 def load_ak_from_csv(csv_file):
+    '''
+    从 csv 中载入为 awkward 数组
+    :param csv_file:
+    :return:
+    '''
     if isinstance(csv_file, str):
         csv_file = open(csv_file, 'r', encoding='utf8')
 
-    # assert isinstance(csv_file, io.FileIO)
     reader = csv.reader(csv_file)
     rows = []
     for row in reader:
@@ -15,3 +19,38 @@ def load_ak_from_csv(csv_file):
 
     arr = ak.Array(rows)
     return arr
+
+
+
+def load_ak_from_pandas(table: pandas.DataFrame):
+    '''
+    从 pandas DataFrame 中载入为 awkward 数组
+    :param table:
+    :return:
+    '''
+    shape = table.shape
+
+    rows = []
+    for y in range(shape[0]):
+        rows.append(table.iloc[y].tolist())
+
+    arr = ak.Array(rows)
+    return arr
+
+
+def load_ak_from_excel(excel_file, sheet_name: str|int|None=0, dtype=str, **pandas_kwargs):
+    '''
+    从 excel 中载入为 awkward 数组
+    :param excel_file: 
+    :param sheet_name: 要载入的表号或表名
+    :return:
+    '''
+    table = pandas.read_excel(excel_file, sheet_name=sheet_name, dtype=dtype, **pandas_kwargs)
+    arr = load_ak_from_pandas(table)
+    return arr
+
+
+if __name__ == '__main__':
+    f = r'a.xlsx'
+    d = load_ak_from_excel(f)
+    print(d)
